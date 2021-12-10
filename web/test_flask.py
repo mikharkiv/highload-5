7ce3d31d2fcd48954816819988287f9f33fef75e
Mykhailo Postnikov <mikharkiv@macpaw.com>
2021-12-10 21:39:15 +0200

    Fix tests

--- web/test_flask.py
+++ web/test_flask.py
@@ -5,15 +5,10 @@ from web.web import create_app
 API_URL = "http://localhost:5000/"
 
 
-def test_healthcheck():
+def test_healthcheck_and_add_book():
     app = create_app()
     client = app.test_client()
     assert client.get("/").status_code == 404
-
-
-def test_add_book():
-    app = create_app()
-    client = app.test_client()
     book = {
         "name": "test",
         "author": "test",
